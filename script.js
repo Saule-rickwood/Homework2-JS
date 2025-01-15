f5335053435f8721797710fa8fff988f7827a3e3
Saule <saulerickwood25@gmail.com>
2025-01-15 17:15:39 +0000

    completed homework 1

--- script.js
+++ script.js
@@ -1,5 +1,66 @@
-let age = Number(prompt('Сколько вам лет?'));
-let up = 33;
-let result = age + up;
+// Задание 1
+let c = 10;
+alert(c);
+c = 20;
+alert(c);
 
-console.log(`in ${up} years you will be ${result}`);
+//Задание 2
+
+const dateIphone = 2007;
+console.log(dateIphone);
+
+//Задание 3
+
+const nameCreatorJS = 'Brendan Eich';
+console.log(nameCreatorJS);
+
+//Задание 4
+
+let l = 10;
+let m = 2;
+console.log(l + m);
+console.log(l - m);
+console.log(l * m);
+console.log(l / m);
+
+//Задание 5
+
+let result = 2 ** 5;
+console.log(`Результат задания номер 5 равен ${result}`);
+
+//Задание  6
+
+let a = 9;
+let b = 2;
+let rest = a % b;
+console.log(rest);
+
+//Задание 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+//Задание 8
+
+let age;
+age = prompt('Сколько вам лет');
+alert(age);
+
+//Задание 9
+
+const user = {
+    name: 'Saule',
+    age: 27,
+    isAdmin: true,
+}
+
+//Задание 10
+
+let userName = prompt('Твое имя');
+alert(`Привет ${userName}!`);
